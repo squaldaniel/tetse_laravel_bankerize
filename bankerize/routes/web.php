e72d3343417554b758c1673655e718c9ae4e0416
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2025-07-10 17:25:25 -0300

    tetse app

--- bankerize/routes/web.php
+++ bankerize/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/proposal', function () {
+    return [
+        'cpf'=> "654564654654",
+        "nome"=> "",
+        "data_nascimento"=>"",
+        "valor emprestimo"=>"",
+        "chave_pix"=>"chave@chave.com"
+    ];
 });
